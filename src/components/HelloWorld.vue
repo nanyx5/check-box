500562bc2c3c0532ee7ad47f8a7e17bb334fa29e
南友新 <nanyx5@chinaunicom.cn>
2022-02-24 09:51:27 +0800

    表单组件初体验

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <i-form ref="form" :model="formValidate" :rules="ruleValidate">
+      <i-form-item label="用户名" prop="name">
+        <i-input v-model="formValidate.name"></i-input>
+      </i-form-item>
+
+      <i-form-item label="邮箱" prop="mail">
+        <i-input v-model="formValidate.mail"></i-input>
+      </i-form-item>
+    </i-form>
+    <button @click="handleSubmit">提交</button>
+    <button @click="handleReset">重置</button>
   </div>
 </template>
 
 <script>
+import iForm from './form/form.vue'
+import iFormItem from './form/form-item.vue'
+import iInput from './input/input.vue'
 export default {
+  components: { iForm, iFormItem, iInput },
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      formValidate: {
+        name: '',
+        mail: ''
+      },
+      ruleValidate: {
+        name: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' }
+        ],
+        mail: [
+          { required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          window.alert('提交成功')
+        } else {
+          window.alert('表单校验失败')
+        }
+      })
+    },
+    handleReset () {
+      this.$refs.form.resetFields()
+    }
+
   }
 }
 </script>
